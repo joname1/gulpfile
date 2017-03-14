fe279090b86ab6b9b2c38d6cf177266581b033fe
joname1 <274158741@qq.com>
2017-03-14 18:22:40 +0800

    fixed

---
+++ README.md
@@ -0,0 +1,22 @@
+## 安装
+
+* `npm install`
+
+你也可以使用yarn来进行安装
+* `yarn install`
+
+## 使用
+
+`gulp css` //CSS压缩
+
+`gulp xb`//雪碧图生成
+
+`gulp js` //JS压缩
+
+`gulp base64` //图片转Base64
+
+`gulp hb` //多个JS、CSS合并为一
+
+`gulp less` //LESS转CSS
+
+`gulp sass` //SASS转CSS
--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,73 @@
-var gulp = require('gulp')
-	minify = require('gulp-minify-css')
-	sprite = require('gulp.spritesmith')
-	uglify = require('gulp-uglify')
-	base64 = require('gulp-base64')
-	imageisux = require('gulp-imageisux')
-	concat = require('gulp-concat')
-	rename = require('gulp-rename')
-	less = require('gulp-less')
-	sass = require('gulp-ruby-sass');
+const gulp = require('gulp')	//gulp插件
+						minify = require('gulp-minify-css')	//CSS压缩
+						sprite = require('gulp.spritesmith')	//雪碧图生成
+						uglify = require('gulp-uglify')	//JS压缩
+						base64 = require('gulp-base64')	//图片转Base64
+						concat = require('gulp-concat')	//多个JS、CSS合并为一
+						rename = require('gulp-rename')	//文件重命名
+						less = require('gulp-less')	//LESS转CSS
+						sass = require('gulp-ruby-sass');	//SASS转CSS
 
 gulp.task('css', function(){
-	return gulp.src('css/*.css')
+	return gulp.src('css/*.css')	//输入路径
 	.pipe(minify())
-	.pipe(gulp.dest('dist'));
+	.pipe(gulp.dest('dist'));	//输出路径
 });
 
 gulp.task('xb', function(){
-	return gulp.src('xb/*.png')
+	return gulp.src('xb/*.png')	//输入路径
 	.pipe(sprite({
 		imgName: 'sprite.png',
 		cssName: 'sprite.css',
 		padding: 10,
 		algorithm: 'binary-tree',
 	}))
-	.pipe(gulp.dest('dist'));
+	.pipe(gulp.dest('dist'));	//输出路径
 });
 
 gulp.task('js', function(){
-	return gulp.src('js/*.js')
+	return gulp.src('js/*.js')	//输入路径
 	.pipe(uglify())
-	.pipe(gulp.dest('dist'));
+	.pipe(gulp.dest('dist'));	//输出路径
 });
 
 gulp.task('b64', function () {
-    return gulp.src('public/css/*.css')
+    return gulp.src('css/*.css')	//输入路径
         .pipe(base64({
             baseDir: 'public',
             extensions: ['png'],
             exclude:    [/\.server\.(com|net)\/dynamic\//, '--live.jpg'],
-            maxImageSize: 8*1024, // bytes 
+            maxImageSize: 8*1024, // bytes
             debug: true
         }))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist'));	//输出路径
 });
- 
+
  gulp.task('base64', ['b64'], function(){
- 	return gulp.src('dist/*.css')
+ 	return gulp.src('dist/*.css')	//输入路径
  	.pipe(minify())
- 	.pipe(gulp.dest('dist/mini'));
+ 	.pipe(gulp.dest('dist'));	//输出路径
  });
 
-gulp.task('imgmi', function(){
-	return gulp.src(['img/*'])
-	.pipe(imageisux('dest',true));
-});
-
 gulp.task('hb', function(){
-	return gulp.src('hb/*.css')
+	return gulp.src('hb/*.css')	//输入路径
 	.pipe(concat('all.css'))
-	.pipe(gulp.dest('dist'))
+	.pipe(gulp.dest('dist'))	//输出路径
 	.pipe(minify())
 	.pipe(rename('bundle.css'))
-	.pipe(gulp.dest('dist'))
+	.pipe(gulp.dest('dist'));	//输出路径
+});
+
+gulp.task('less', function () {
+    gulp.src('less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('sass', function() {
+    return sass('sass/')
+    .on('error', function (err) {
+      console.error('Error!', err.message);
+   })
+    .pipe(gulp.dest('dist'))
 });
